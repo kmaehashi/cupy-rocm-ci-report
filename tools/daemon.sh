212996db97abddcf572aa648cdb9f9e1660ea0eb
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2021-01-31 20:58:57 -0800

    fix

--- tools/daemon.sh
+++ tools/daemon.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-set -uex
-
 while :; do
     echo "<<< $(date) >>> Starting..."
-    # max 8 hours for test session
-    srun -t 0-8 "$(dirname "$0")/test_runner.sh"
+    srun "$(dirname "$0")/test_runner.sh"
     echo "<<< $(date) >>> Finished..."
-    sleep $((3600 * 12))
+    sleep $((3600))
 done
